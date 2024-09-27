82ea44728dff81a2b52e808baa77d7357e3d8ef5
hoodmanb <joshuadebravo@gmail.com>
2024-09-27 22:51:15 +0100

    added

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+
+        this.client.on('error', (err) => {
+            console.error(err);
+        });
+
+        this.client.connect().catch((err) => {
+            console.error(err);
+        });
+    }
+
+    isAlive = async () => {
+        const result = await this.client.ping()
+            .then((ping) => ping === 'PONG')
+            .catch((err) => {
+                console.error(err);
+                return false;
+            });
+        return result;
+    };
+
+
+    async get(key) {
+        try {
+            const value = await this.client.get(key);
+            return value;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async set(key, value, duration) {
+        try {
+            // Convert value to string before storing in Redis
+            await this.client.setEx(key, duration, String(value));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.client.del(key);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
